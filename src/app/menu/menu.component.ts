9a5fcd6a0aeb80b75971ba0e4e3de4fb7f253d4c
anamonsalve <anamonsalve96@gmail.com>
2023-06-09 01:04:15 -0500

    mini cambio

--- src/app/menu/menu.component.ts
+++ src/app/menu/menu.component.ts
@@ -1,39 +1,28 @@
-import { Component, HostListener, Renderer2 } from '@angular/core';
+import { Component,HostListener, Renderer2 } from '@angular/core';
 
 @Component({
   selector: 'app-menu',
   templateUrl: './menu.component.html',
   styleUrls: ['./menu.component.css']
 })
-export class MenuComponent { 
-  
-  public scrolled: boolean = false;
+export class MenuComponent {
 
-  constructor(private renderer: Renderer2) { }
+  public scrolliando:boolean=false
 
-  @HostListener ('window:scroll')
-  onWindowScroll() {
-    // Lógica para cambiar el estilo del menú
+  constructor(private render:Renderer2){}
 
-    const elemento = document.getElementById('menu')
-    if (window.pageYOffset > 0) {
-      this.scrolled = true;
-      console.log(elemento)
-      this.renderer.addClass(elemento, 'menu2');
-      this.renderer.removeClass(elemento, 'menu');
-    } else {
-      this.scrolled = false;
-      console.log(elemento)
-      this.renderer.removeClass(elemento, 'menu2');
-      this.renderer.addClass(elemento, 'menu');
+  @HostListener('window:scroll')
+  onWindowScroll(){
+    let elemento=document.getElementById("menu")
+    if(window.pageYOffset>0){
+      this.scrolliando=true
+      this.render.addClass(elemento,'menu2')
+      this.render.removeClass(elemento,'menu')
+    }else{
+      this.scrolliando=false
+      this.render.removeClass(elemento,'menu2')
+      this.render.addClass(elemento,'menu')
     }
   }
-
-  @HostListener('window:resize')
-  onWindowResize() {
-    // Se vuelve a verificar el estado del scroll cuando se cambia el tamaño de la ventana
-    this.onWindowScroll();
-  }
-
 
 }
